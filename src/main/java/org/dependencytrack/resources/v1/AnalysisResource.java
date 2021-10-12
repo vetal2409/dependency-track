ea4a4f909f497a2913e0ab0e5842c10af0408ba7
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-10-12 18:00:31 +0300

    feat: [modified] added trigger of the metrics update for project after suppression of the vulnerability

--- src/main/java/org/dependencytrack/resources/v1/AnalysisResource.java
+++ src/main/java/org/dependencytrack/resources/v1/AnalysisResource.java
@@ -18,6 +18,7 @@
  */
 package org.dependencytrack.resources.v1;
 
+import alpine.event.framework.Event;
 import alpine.common.validation.RegexSequence;
 import alpine.common.validation.ValidationTask;
 import alpine.model.LdapUser;
@@ -34,6 +35,7 @@ import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import org.apache.commons.lang3.StringUtils;
 import org.dependencytrack.auth.Permissions;
+import org.dependencytrack.event.MetricsUpdateEvent;
 import org.dependencytrack.model.Analysis;
 import org.dependencytrack.model.AnalysisJustification;
 import org.dependencytrack.model.AnalysisResponse;
@@ -196,6 +198,10 @@ public class AnalysisResource extends AlpineResource {
             qm.makeAnalysisComment(analysis, comment, commenter);
             analysis = qm.getObjectById(Analysis.class, analysis.getId());
             NotificationUtil.analyzeNotificationCriteria(qm, analysis, analysisStateChange, suppressionChange);
+
+            final Project detachedProject = qm.detach(Project.class, project.getId());
+            Event.dispatch(new MetricsUpdateEvent(detachedProject));
+
             return Response.ok(analysis).build();
         }
     }
