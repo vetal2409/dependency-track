55a04349f024c1d2a6b1832c51a61639904cb614
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-12-01 17:49:28 +0200

    feat: added GlobalExceptionHandler for REST API

---
+++ src/main/java/org/dependencytrack/resources/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.dependencytrack.resources;
+
+import alpine.common.logging.Logger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Exception>
+{
+    private static final Logger LOGGER = Logger.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    public Response toResponse(Exception exception)
+    {
+        LOGGER.error("Uncaught internal server error", exception);
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Uncaught internal server error").build();
+    }
+}
